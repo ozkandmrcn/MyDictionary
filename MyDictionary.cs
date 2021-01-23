40e4b40f6c072f9b4236f220d4b18ce41bbeab9a
ozkandemiircan@gmail.com <ozkandemiircan@gmail.com>
2021-01-23 23:44:27 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        public K[] keys;
+        public V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] temporaryKeys = keys;
+            V[] temporaryValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < temporaryKeys.Length; i++)
+            {
+                keys[i] = temporaryKeys[i];
+                values[i] = temporaryValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customers = new MyDictionary<int, string>();
+
+            customers.Add(1, "Ali");
+            customers.Add(2, "Veli");
+            customers.Add(3, "Ayşe");
+            customers.Add(4, "Meltem");
+
+            for (int i = 0; i < customers.keys.Length; i++)
+            {
+                Console.WriteLine("Müşteri Numarası: " + customers.keys[i] + "\n" + "Müşteri Adı: " + customers.values[i] + "\n" + "**************");
+            }
+        }
+    }
+}
